760be6e4fe1932cbda6f8cab2f3cad4b6e8c321a
staxxtec <144523338+staxxtec@users.noreply.github.com>
2024-02-20 19:05:13 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-document.getElementById("count-el").innerText = 5
+//document.getElementById("count-el").innerText = 5
+
+// let firstBatch = 5
+// let secondBatch = 7
+
+// let count = firstBatch + secondBatch
+
+
+// console.log(count);
+
+
+
+
+
+
+
+
+
+
+
