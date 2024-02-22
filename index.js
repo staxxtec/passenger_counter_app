9e16bb6eb7a5cedddb0b2e0531ae1d6bea7a14b9
staxxtec <144523338+staxxtec@users.noreply.github.com>
2024-02-22 15:38:43 +0900

    Update index.js

--- index.js
+++ index.js
@@ -21,8 +21,10 @@ function increment() {
 function save() {
     let countStr = count + " - "
     saveEl.textContent += countStr
+    countEl.textContent = 0
+    count = 0
 
-    console.log(count)
+    
 }
 
 
